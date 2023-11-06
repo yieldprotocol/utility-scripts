1709258e89e4de220d69d296efd076a1a5f33bfa
brucedonovan <bruce.donovan@gmail.com>
2023-11-06 15:08:48 +0000

    handle chainID

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Fetches all the token holders and balances ( >0) for a list of tokens.
 Usage: 
 1. Add token addresses to the ```./src/getAllTokenHolders/tokenList.json``` file.
 2. run ```yarn && yarn getAllTokenHolders``` 
+
+
+## getAllVaults
+Fetches all the vaults that were built in a particular cauldron.
+Usage: 
+1. Set to the desired chainId in the  ```./src/getAllVaults/chainId.json``` file. (currently only "1" or "42161")
+2. run ```yarn && yarn getAllVaults``` 
--- src/getAllVaults/getAllVaults.ts
+++ src/getAllVaults/getAllVaults.ts
@@ -1,6 +1,7 @@
 import { AbstractProvider, JsonRpcProvider, ethers } from "ethers";
 import fs from "fs";
 import csvWriter from "csv-write-stream";
+import { chainId } from "./chainId.json";
 
 require("dotenv").config();
 
@@ -11,10 +12,13 @@ export interface Vault {
   seriesId: string;
 }
 
-async function getAllVaults_(provider: AbstractProvider): Promise<Vault[]> {
+async function getAllVaults_(
+  cauldronAddress: string,
+  provider: AbstractProvider
+): Promise<Vault[]> {
   // const vaults = [] as Vault[];
   const cauldronContract = new ethers.Contract(
-    "0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867", // '0x23cc87FBEBDD67ccE167Fa9Ec6Ad3b7fE3892E30'
+    cauldronAddress,
     [
       "event VaultBuilt(bytes12 indexed vaultId, address indexed owner, bytes6 indexed seriesId, bytes6 ilkId)",
     ],
@@ -22,10 +26,15 @@ async function getAllVaults_(provider: AbstractProvider): Promise<Vault[]> {
   );
 
   /* Get a list of the vaults that were BUILT */
-  const vaultsBuiltFilter = cauldronContract.filters.VaultBuilt(null, null, null, null);
-  const vaultsBuilt = await cauldronContract.queryFilter(vaultsBuiltFilter!);
+  const vaultsBuiltFilter = cauldronContract.filters.VaultBuilt(
+    null,
+    null,
+    null,
+    null
+  );
+  const vaultsBuilt = await cauldronContract.queryFilter(vaultsBuiltFilter!); // 0x5E7777, 0x92E103
 
-  const vaults = vaultsBuilt.map((x:any) => {
+  const vaults = vaultsBuilt.map((x: any) => {
     const { vaultId: id, ilkId, seriesId, owner } = x.args;
     return {
       id,
@@ -37,28 +46,38 @@ async function getAllVaults_(provider: AbstractProvider): Promise<Vault[]> {
   return vaults;
 }
 
-export const getAllVaults = async (writeToCSVFile: boolean = false) => {
-  const provider = new JsonRpcProvider(
-    `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
-  );
+export const getAllVaults = async (
+  chainId: string,
+  writeToCSVFile: boolean = false
+) => {
+  const chainInfo =
+    chainId === "1"
+      ? {
+          cauldronAddress: "0xc88191F8cb8e6D4a668B047c1C8503432c3Ca867",
+          provider: new JsonRpcProvider(
+            `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+          ),
+        }
+      : {
+          cauldronAddress: "0x23cc87FBEBDD67ccE167Fa9Ec6Ad3b7fE3892E30",
+          provider: new JsonRpcProvider(
+            `https://arbitrum-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+          ),
+        };
 
-  const vaults = await getAllVaults_(provider);
+  const vaults = await getAllVaults_(
+    chainInfo.cauldronAddress,
+    chainInfo.provider
+  );
 
   /* write token holders to csv file */
   if (writeToCSVFile) {
     const writer = csvWriter({
       headers: ["Owner", "ilkId", "seriesId", "id"],
     });
-    writer.pipe(
-      fs.createWriteStream("./src/getAllVaults/output/vaults.csv")
-    );
+    writer.pipe(fs.createWriteStream("./src/getAllVaults/output/vaults.csv"));
     vaults.forEach((vault: Vault) => {
-      writer.write([
-        vault.owner,
-        vault.ilkId,
-        vault.seriesId,
-        vault.id,
-      ]);
+      writer.write([vault.owner, vault.ilkId, vault.seriesId, vault.id]);
     });
     writer.end();
   }
@@ -69,4 +88,4 @@ export const getAllVaults = async (writeToCSVFile: boolean = false) => {
   return vaults;
 };
 
-getAllVaults(true);
+getAllVaults(chainId, true);
